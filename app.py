16083ad9ffcf601c87a612c468d082bfbf5e9d48
tel0952502642 <tel0952502642@gmail.com>
2025-07-08 05:04:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify
 import sheet_writer
 
@@ -34,53 +33,3 @@ def delete():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-@app.route('/weight', methods=['POST'])
-def add_weight():
-    data = request.get_json()
-    result = sheet_writer.add_weight(data)
-    return jsonify(result)
-
-@app.route('/weight/query', methods=['GET'])
-def query_weight():
-    date = request.args.get("date")
-    result = sheet_writer.query_weight(date)
-    return jsonify(result)
-
-@app.route('/weight/update', methods=['POST'])
-def update_weight():
-    data = request.get_json()
-    result = sheet_writer.update_weight(data)
-    return jsonify(result)
-
-@app.route('/weight/delete', methods=['POST'])
-def delete_weight():
-    data = request.get_json()
-    result = sheet_writer.delete_weight(data)
-    return jsonify(result)
-
-
-@app.route('/poop', methods=['POST'])
-def add_poop():
-    data = request.get_json()
-    result = sheet_writer.add_poop(data)
-    return jsonify(result)
-
-@app.route('/symptom', methods=['POST'])
-def add_symptom():
-    data = request.get_json()
-    result = sheet_writer.add_symptom(data)
-    return jsonify(result)
-
-@app.route('/exercise', methods=['POST'])
-def add_exercise():
-    data = request.get_json()
-    result = sheet_writer.add_exercise(data)
-    return jsonify(result)
-
-
-@app.route('/supplement', methods=['POST'])
-def add_supplement():
-    data = request.get_json()
-    result = sheet_writer.add_supplement(data)
-    return jsonify(result)
