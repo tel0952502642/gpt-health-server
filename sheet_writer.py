19166c911568b49c8f35d0b36ad054ee910c9b3b
tel0952502642 <tel0952502642@gmail.com>
2025-07-10 03:39:25 +0800

    Update sheet_writer.py

--- sheet_writer.py
+++ sheet_writer.py
@@ -3,14 +3,13 @@ import json
 import gspread
 from google.oauth2.service_account import Credentials
 
-# 設定 Scope（重要！否則會出現 invalid_scope 錯誤）
-scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+# 從環境變數讀取 service account JSON
 creds_info = json.loads(os.environ["GOOGLE_CREDENTIALS"])
-creds = Credentials.from_service_account_info(creds_info, scopes=scopes)
+creds = Credentials.from_service_account_info(creds_info)
 gc = gspread.authorize(creds)
 
-# Google Sheet ID 與 工作表名稱
-SHEET_ID = "1vKHAW5WjUqdveQ6uwgQxNbbKagdFh-ZxeEXp-i2mQzo"
+# Google Sheet ID 與工作表名稱
+SHEET_ID = "1vKH4W5WjUqdveQ6uwgQxNbbKagdFh-ZxeEXp-i2mQzo"
 SHEET_NAME = "吃食紀錄表"
 
 def append_row(data):
